c725b5bbcd40c30aa993b672170d645bdd04948c
geestaq <gee@vp.pl>
2019-08-11 19:39:36 +0200

    Add player's score test for minus button

--- src/components/Player/Player.test.js
+++ src/components/Player/Player.test.js
@@ -23,3 +23,25 @@ it('renders correct score', () => {
 
   expect(playerScoreRendered).toEqual(playerScorePassed);
 });
+
+it('should call onPlayerScoreChange with 1 when plus button is clicked', () => {
+  const mockedOnPlayerScoreChange = jest.fn();
+  const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+
+  const plusButton = playerComponent.find('.Player__button_plus1');
+
+  plusButton.simulate('click');
+
+  expect(mockedOnPlayerScoreChange).toBeCalledWith(1);
+});
+
+it('should call onPlayerScoreChange with -1 when minus button is clicked', () => {
+  const mockedOnPlayerScoreChange = jest.fn();
+  const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+
+  const minusButton = playerComponent.find('.Player__button_minus1');
+
+  minusButton.simulate('click');
+
+  expect(mockedOnPlayerScoreChange).toBeCalledWith(-1);
+});
